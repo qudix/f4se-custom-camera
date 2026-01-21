64238f7f53e298131e4effc6b6104f2e2093e388
Qudix <17361645+Qudix@users.noreply.github.com>
2026-01-20 22:15:32 -0600

    feat: update lib

--- xmake.lua
+++ xmake.lua
@@ -1,16 +1,10 @@
-set_xmakever("2.8.2")
-
 includes("lib/commonlibf4")
 
 set_project("f4se-custom-camera")
 set_version("0.5.4")
 set_license("GPL-3.0")
-
 set_languages("c++23")
 set_warnings("allextra")
-set_defaultmode("releasedbg")
-
-set_policy("package.requires_lock", true)
 
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
@@ -18,8 +12,6 @@ add_rules("plugin.vsxmake.autoupdate")
 add_requires("simpleini")
 
 target("f4se-custom-camera")
-    add_deps("commonlibf4")
-
     add_packages("simpleini")
 
     add_rules("commonlibf4.plugin", {
